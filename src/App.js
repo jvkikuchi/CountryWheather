08cdf58392b0127c1b88b6452676931cbc2b1336
João Victor Kikuchi <60197143+jvkikuchi@users.noreply.github.com>
2022-01-25 21:18:08 -0300

    commit from home

--- src/App.js
+++ src/App.js
@@ -6,21 +6,15 @@ const App = () => {
   const [countries, setCountries] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
   const [search, setSearch] = useState("");
-  const [weather, setWeather] = useState([])
 
-  c
+
 
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
       setCountries(response.data);
       console.log(response.data.length, "COUNTRIES FETCHED");
     });
-
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${}&appid=781bcf355de8e4981f7c177f5e119188`)
-         .then((response) => {
-      setWeather(response.data);
-      console.log("WEATHER FETCHED");
-    });
+    
   }, []);
 
   const handleSearch = (event) => {
@@ -30,14 +24,12 @@ const App = () => {
         country.name.common.toLowerCase().includes(event.target.value)
       );
     setFilteredCountries(filteredCoutry);
-    console.log(filteredCountries);
   };
 
   return (
     <div>
-      {console.log(weather)}
       find countries <input value={search} onChange={handleSearch}></input>
-      <Filter countries={filteredCountries} search={search} handleShow={setFilteredCountries} />
+      <Filter countries={filteredCountries} search={search} handleShow={setFilteredCountries}/>
     </div>
   );
 };
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,12 +1,41 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const Country = ({country}) =>{
+  const [weathers, setWeathers] = useState([])
+
+  useEffect(() => {
+    async function getWeather() {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=781bcf355de8e4981f7c177f5e119188`)
+      setWeathers(response.data)
+      console.log(response.data)
+      
+    }
+
+    getWeather();
+  }, [])
+
+  // useEffect(() => {
+  //     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=&appid=781bcf355de8e4981f7c177f5e119188`)
+  //        .then((response) => {
+  //        setWeathers(response.data)
+  //        console.log(weathers, 'WEATHER FETCHED')
+  //   });
+    
+  // }, []);
+
+
   return(
     <div>
       <h1>{country.name.common}</h1>
       <p>official name: {country.name.official}</p>
       <p>capital: {country.capital}</p>
       <p>population: {country.population}</p>
-      <h2>Flag</h2>
+      <h2>Flag</h2> 
       <img src={country.flags.png} alt="country flag"></img>
+      <h2>Current Weather</h2>
+      <p>wind speed: {weathers.wind.speed}</p>      
+     
     </div>
   )
 }
